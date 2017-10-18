493b6ed9c417d505dabd03a2c5a1d972e91e08ec
Jeffrey David Payne <jeffreydpayne@Jeffreys-MacBook-Pro.local>
2017-10-18 10:38:11 -0700

    Cleaning up unused imports.

--- src/main/java/edu/bls/site/config/ThymeLeafConfiguration.java
+++ src/main/java/edu/bls/site/config/ThymeLeafConfiguration.java
@@ -1,7 +1,5 @@
 package edu.bls.site.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
